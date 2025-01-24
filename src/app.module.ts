8b111338b023d1f17c2b882d5f9f03e19e28389d
subhankar-fleapo <subhankar@fleapo.com>
2025-01-25 02:11:33 +0530

    feat: project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
+import { DatabaseModule } from '@data/database.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule],
 })
 export class AppModule {}
