6781064dde9898bbb812b98202ccc63cfb6ad5cf
soumalya-fleapo <soumalya.ghoshal@fleapo.com>
2025-01-26 20:48:44 +0530

    feat: setup knip

---
+++ knip.config.ts
@@ -0,0 +1,11 @@
+import { KnipConfig } from 'knip';
+
+const config: KnipConfig = {
+  rules: {
+    dependencies: 'off',
+    devDependencies: 'off',
+    unlisted: 'off',
+  },
+};
+
+export default config;
